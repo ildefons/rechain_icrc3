76e0c5425eeeee5b6d3d25e0266606f1afd0e416
infu <vtsukev@gmail.com>
2024-07-15 16:17:12 +0300

    delta tests

---
+++ test/delta.spec.ts
@@ -0,0 +1,209 @@
+import { Principal } from '@dfinity/principal';
+import { resolve } from 'node:path';
+import { Actor, PocketIc, createIdentity } from '@hadronous/pic';
+import { IDL } from '@dfinity/candid';
+import { _SERVICE as TestService, idlFactory as TestIdlFactory, init, ArchivedTransactionResponse, GetBlocksResult } from './build/delta.idl.js';
+
+// ILDE import {ICRCLedgerService, ICRCLedger} from "./icrc_ledger/ledgerCanister";
+//@ts-ignore
+import {toState} from "@infu/icblast";
+// Jest can't handle multi threaded BigInts o.O That's why we use toState
+
+const WASM_PATH = resolve(__dirname, "./build/delta.wasm");
+
+export async function TestCan(pic:PocketIc) {
+    
+    const fixture = await pic.setupCanister<TestService>({
+        idlFactory: TestIdlFactory,
+        wasm: WASM_PATH,
+        arg: IDL.encode(init({ IDL }), []),
+    });
+
+    return fixture;
+};
+
+
+describe('Delta', () => {
+    let pic: PocketIc;
+    let can: Actor<TestService>;
+    let canCanisterId: Principal;
+
+    // const jo = createIdentity('superSecretAlicePassword');
+    // const bob = createIdentity('superSecretBobPassword');
+  
+    beforeAll(async () => {
+      pic = await PocketIc.create(process.env.PIC_URL); 
+      const fixture = await TestCan(pic);
+      can = fixture.actor;
+      canCanisterId = fixture.canisterId; 
+
+      await can.set_ledger_canister();
+    });
+  
+    afterAll(async () => {
+      await pic.tearDown();  //ILDE: this means "it removes the replica"
+    });
+  
+    it('empty dispatch', async () => {
+      
+      let r = await can.dispatch([]);
+      expect(r.length).toBe(0);
+    });
+
+    it('empty log', async () => {
+        let rez = await can.icrc3_get_blocks([{
+            start: 0n,
+            length: 100n
+        }]);
+        expect(rez.archived_blocks.length).toBe(0);
+        expect(rez.blocks.length).toBe(0);
+        expect(rez.log_length).toBe(0n);
+    });
+
+    it("dispatch 1 action", async () => {
+
+      let r = await can.dispatch([{
+        ts:12340n,
+        created_at_time: 1721045569580000n,
+        memo: [0,1,2,3,4],
+        caller: Principal.fromText("eqsml-lyaaa-aaaaq-aacdq-cai"),
+        fee: 1000n,
+        payload: {
+          swap : {amt: 123456n}
+        }
+      }]);
+      expect(toState(r[0]).Ok).toBe("1");
+    });
+
+    it('icrc3_get_blocks with 1 block', async () => {
+      let rez = await can.icrc3_get_blocks([{
+          start: 0n,
+          length: 100n
+      }]);
+      let strblock = JSON.stringify(toState(rez.blocks[0]));
+      expect(strblock).toBe('{"id":"0","block":[{"Map":[["tx",{"Map":[["ts",{"Nat":"12340"}],["created_at_time",{"Nat":"1721045569580000"}],["memo",{"Blob":"0001020304"}],["caller",{"Blob":"00000000020000870101"}],["fee",{"Nat":"1000"}],["btype",{"Text":"1swap"}],["payload",{"Map":[["amt",{"Nat":"123456"}]]}]]}]]}]}');
+      // expect(rez.log_length).toBe(1n);
+      expect(rez.archived_blocks.length).toBe(0);
+      expect(rez.blocks.length).toBe(1);
+      
+    });
+
+
+    it("dispatch 300 actions in 300 calls", async () => {
+      for (let i=0; i<300; i++) {
+        let r = await can.dispatch([{
+          ts:12340n,
+          created_at_time: 1721045569580000n,
+          memo: [0,1,2,3,4],
+          caller: Principal.fromText("eqsml-lyaaa-aaaaq-aacdq-cai"),
+          fee: 1000n,
+          payload: {
+            swap : {amt: 123456n}
+          }
+        }]);
+        expect(toState(r[0]).Ok).toBe((2 + i).toString());
+      }
+     await passTime(20);
+    });
+
+
+    it('icrc3_get_blocks 100', async () => {
+      let rez = await can.icrc3_get_blocks([{
+          start: 0n,
+          length: 100n
+      }]);
+
+      // console.log(JSON.stringify(toState(rez.archived_blocks)));
+
+
+      let archive_rez = await getArchived(rez.archived_blocks[0]);
+      
+      expect(archive_rez.blocks[5].id).toBe(5n);
+      
+    });
+
+    async function getArchived(arch_param:ArchivedTransactionResponse) : Promise<GetBlocksResult> {
+      let archive_principal = Principal.fromText(toState(arch_param).callback[0]);
+      const archive_actor = pic.createActor<TestService>(TestIdlFactory, archive_principal);
+      let args = arch_param.args[0]
+      return await archive_actor.icrc3_get_blocks([args]);
+    }
+
+      it('icrc3_get_blocks first 301', async () => {
+      let rez = await can.icrc3_get_blocks([{
+          start: 0n,
+          length: 500n
+      }]);
+
+      
+      expect(rez.blocks[0].id).toBe(240n);
+      expect(rez.blocks[ rez.blocks.length - 1].id).toBe(300n);
+
+      let archive_rez_0 = await getArchived(rez.archived_blocks[0]);
+      expect(archive_rez_0.blocks[0].id).toBe(0n);
+      expect(archive_rez_0.blocks[ archive_rez_0.blocks.length - 1].id).toBe(119n);
+
+      let archive_rez_1 = await getArchived(rez.archived_blocks[1]);
+      expect(archive_rez_1.blocks[0].id).toBe(120n);
+      expect(archive_rez_1.blocks[ archive_rez_1.blocks.length - 1].id).toBe(239n);
+
+    });
+
+
+
+    it("dispatch 300 actions in 1 call", async () => {
+        let r = await can.dispatch(Array.from({ length: 300 }, () => ({
+          ts:12340n,
+          created_at_time: 1721045569580000n,
+          memo: [0,1,2,3,4],
+          caller: Principal.fromText("eqsml-lyaaa-aaaaq-aacdq-cai"),
+          fee: 1000n,
+          payload: {
+            swap : {amt: 123456n}
+          }
+        })));
+        expect(toState(r[0]).Ok).toBe("302");
+        await passTime(20);
+    });
+
+
+    it('icrc3_get_blocks 601', async () => {
+      let rez = await can.icrc3_get_blocks([{
+          start: 0n,
+          length: 800n
+      }]);
+
+
+      expect(rez.blocks[0].id).toBe(571n);
+      expect(rez.blocks[ rez.blocks.length - 1].id).toBe(600n);
+
+      let archive_rez_0 = await getArchived(rez.archived_blocks[0]);
+      expect(archive_rez_0.blocks[0].id).toBe(0n);
+      expect(archive_rez_0.blocks[ archive_rez_0.blocks.length - 1].id).toBe(119n);
+
+      let archive_rez_1 = await getArchived(rez.archived_blocks[1]);
+      expect(archive_rez_1.blocks[0].id).toBe(120n);
+      expect(archive_rez_1.blocks[ archive_rez_1.blocks.length - 1].id).toBe(239n);
+
+      let archive_rez_2 = await getArchived(rez.archived_blocks[2]);
+      expect(archive_rez_2.blocks[0].id).toBe(240n);
+      expect(archive_rez_2.blocks[ archive_rez_2.blocks.length - 1].id).toBe(359n);
+
+      let archive_rez_3 = await getArchived(rez.archived_blocks[3]);
+      expect(archive_rez_3.blocks[0].id).toBe(360n);
+      expect(archive_rez_3.blocks[ archive_rez_3.blocks.length - 1].id).toBe(479n);
+
+      let archive_rez_4 = await getArchived(rez.archived_blocks[4]);
+      expect(archive_rez_4.blocks[0].id).toBe(480n);
+      expect(archive_rez_4.blocks[ archive_rez_4.blocks.length - 1].id).toBe(570n);
+
+    });
+
+    async function passTime(n:number) {
+    for (let i=0; i<n; i++) {
+        await pic.advanceTime(3*1000);
+        await pic.tick(2);
+      }
+    }
+
+});
--- test/delta.test.mo
+++ test/delta.test.mo
@@ -1,14 +1,12 @@
 import Map "mo:map/Map";
 import Principal "mo:base/Principal";
-import U "../src/utils";
 import Array "mo:base/Array";
 import Blob "mo:base/Blob";
 import SWB "mo:swb/Stable";
 import Debug "mo:base/Debug";
 import Nat "mo:base/Nat";
-import T "../src/types";
 import Sha256 "mo:sha2/Sha256";
-import rechainIlde "../src/rechainIlde";
+import rechain  "../src/rechain";
 import Vec "mo:vector";
 import Nat64 "mo:base/Nat64";
 import RepIndy "mo:rep-indy-hash";
@@ -18,17 +16,17 @@ import Iter "mo:base/Iter";
 
 actor Self {
 
-    public type ActionIlde = {
+    public type Action = {
         ts: Nat64;
         created_at_time: Nat64;
         memo: Blob;
         caller: Principal;
         fee: Nat;
         payload : {
-            #swap : {
+            #swap: {
                 amt: Nat;
             };
-            #add : {
+            #add: {
                 amt : Nat;
             };
         };
@@ -36,86 +34,68 @@ actor Self {
 
     public type ActionError = {ok:Nat; err:Text};
 
-    stable let chain_mem_ilde = rechainIlde.MemIlde();
+    stable let chain_mem  = rechain.Mem();
 
-    func encodeBlock(b: ActionIlde) : rechainIlde.BlockIlde {
+    func encodeBlock(b: Action): rechain.Value {
 
-        let trx : T.BlockIlde = #Map([
+        let trx: rechain.Value  = #Map([
             ("ts", #Nat(Nat64.toNat(b.ts))),
             ("created_at_time", #Nat(Nat64.toNat(b.created_at_time))),
             ("memo", #Blob(b.memo)),
             ("caller", #Blob(Principal.toBlob(b.caller))),
             ("fee", #Nat(b.fee)),
             ("btype", #Text(switch (b.payload) {
-            case (#swap(_)) "1swap";
-            case (#add(_)) "1add";
+                case (#swap(_)) "1swap";
+                case (#add(_)) "1add";
             })),
             ("payload", #Map(switch (b.payload) {
-            case (#swap(data)) {
-                [
-                    ("amt", #Nat(data.amt))
-                ]
-            };
-            case (#add(data)) {
-                [
-                    ("amt", #Nat(data.amt))
-                ]
-            };
+                case (#swap(data)) {
+                    [
+                        ("amt", #Nat(data.amt))
+                    ]
+                };
+                case (#add(data)) {
+                    [
+                        ("amt", #Nat(data.amt))
+                    ]
+                };
             }))
         ]);
 
     };
 
-    func hashBlock(b: rechainIlde.BlockIlde) : Blob {
+    func hashBlock(b: rechain.Value ): Blob {
         Blob.fromArray(RepIndy.hash_val(b));
     };
 
-    
-    public query func icrc3_get_blocks(args: T.GetBlocksArgs) : async T.GetBlocksResult{
-        return chain_ilde.get_blocks(args);
+    public query func icrc3_get_blocks(args: rechain.GetBlocksArgs): async rechain.GetBlocksResult {
+        return chain.get_blocks(args);
     };
 
-    public query func icrc3_get_archives(args: T.GetArchivesArgs) : async T.GetArchivesResult{
-        return chain_ilde.get_archives(args);
+    public query func icrc3_get_archives(args: rechain.GetArchivesArgs): async rechain.GetArchivesResult {
+        return chain.get_archives(args);
     };
 
-    var chain_ilde = rechainIlde.ChainIlde<ActionIlde, ActionError>({ 
+    public shared(msg) func set_ledger_canister(): async () {
+        chain_mem.canister := ?Principal.fromActor(Self);
+        //chain.set_ledger_canister(Principal.fromActor(Self));
+    };
+
+    var chain = rechain.Chain<Action, ActionError>({
         args = null;
-        mem = chain_mem_ilde;
-        encodeBlock = encodeBlock;//func(b: T.ActionIlde) = #Blob("0" : Blob); //("myschemaid", to_candid (b)); // ERROR: this is innecessary. We need to retrieve blocks
-                                                            // action.toGenericValue: I have to write it
-                                                            // it converts the action to generic value!!!
-                                                            // it converts to the action type to generic "value" type
-                                                            // "to_candid" is different implementation in different languages
-                                                            // instead  
-                                                            // !!!! maybe the order of functions inside the dispatch of the rechain we need to re-order 
-        addPhash = func(a, phash) = #Blob("0" : Blob); //{a with phash};            // !!!! RROR because I type is wrong above?
-        hashBlock = hashBlock;//func(b) = Sha256.fromBlob(#sha224, "0" : Blob);//b.1);   // NOT CORRECT: I should hash according to ICERC3 standard (copy/learn from ICDev)
-        // reducers = [balancesIlde.reducer, dedupIlde.reducer];//, balancesIlde.reducer];      //<-----REDO
+        mem = chain_mem;
+        encodeBlock = encodeBlock;
+        addPhash = func(a, phash) = #Blob("0" : Blob);
+        hashBlock = hashBlock;
+        // reducers = [balances .reducer, dedup .reducer];//, balances .reducer];      //<-----REDO
         reducers = [];
     });
-    
 
-    public shared(msg) func init(): () {
-        chain_mem_ilde.canister := ?Principal.fromActor(Self);
-    };
 
-    public shared(msg) func add_record(x: ActionIlde): () {
-        //return icrc3().add_record<system>(x, null);
+    public type DispatchResult = {#Ok: rechain.BlockId; #Err: ActionError };
 
-        let ret = chain_ilde.dispatch(x);  //handle error
-        //add block to ledger
-        switch (ret) {
-            case (#Ok(p)) {
-                //Debug.print("Ok");
-                //return p;
-            };
-            case (#Err(p)) {
-                //<---I MHERE WHY????  Reducer BalcerIlde is giving error
-                Debug.print("Error");
-                //return 0;
-            }
-        }; 
+    public shared(msg) func dispatch(actions: [Action]): async [DispatchResult] {
+        Array.map(actions, func(x:Action): DispatchResult = chain.dispatch(x));
     };
 
 
