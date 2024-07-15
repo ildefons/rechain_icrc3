0e75b29139c7bc2dd16009827a2673a64f10b59f
infu <vtsukev@gmail.com>
2024-07-15 16:20:29 +0300

    gi

--- test/build/basic.idl.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface anon_class_2_1 { 'test' : () => Promise<bigint> }
-export interface _SERVICE extends anon_class_2_1 {}
--- test/build/basic.idl.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const anon_class_2_1 = IDL.Service({ 'test' : IDL.Func([], [IDL.Nat], []) });
-  return anon_class_2_1;
-};
-export const init = ({ IDL }) => {
-  return [IDL.Record({ 'ledgerId' : IDL.Principal })];
-};
